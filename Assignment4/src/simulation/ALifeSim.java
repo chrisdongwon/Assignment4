4031a3b93b4bb19caee41807e0fbcb33f119fa84
zixuan <guozixua@grinnell.edu>
2019-02-20 22:17:52 -0600

    first implement

---
+++ Assignment4/src/simulation/ALifeSim.java
@@ -0,0 +1,42 @@
+package simulation;
+
+public class ALifeSim {
+  public static void main(String[] args) {
+    int iteration = Integer.valueOf(args[0]);
+    int numOfCooperator = Integer.valueOf(args[1]);
+    int numOfDefector = Integer.valueOf(args[2]);
+    int numOfPartial = Integer.valueOf(args[3]);
+    
+    Pair<String, Integer>[] pairs = (Pair<String, Integer>[]) new Pair[3];
+    pairs[0] = new Pair<String, Integer>("Cooperator", numOfCooperator);
+    pairs[1] = new Pair<String, Integer>("Defector", numOfDefector);
+    pairs[2] = new Pair<String, Integer>("PartialCooperator", numOfPartial);
+    
+    Population p = new Population(pairs);
+    for (int i = 0; i < iteration; i++) {
+      p.update();
+    }
+    Pair<String, Integer>[] results = p.getPopulationCounts();
+    Integer finalCooperator = 0;
+    Integer finalDefector = 0;
+    Integer finalPartial = 0;
+    for (Pair<String, Integer> org : results) {
+      if (org.left.equals("Cooperator")) {
+        finalCooperator = org.right;
+      } else if (org.left.equals("Defector")) {
+        finalDefector = org.right;
+      } else if (org.left.equals("PartialCooperator")) {
+        finalPartial = org.right;
+      } else {
+        System.out.println("CRITIAL ERROR!");
+      }
+    }
+    
+    System.out.println("After " + iteration + " ticks:");
+    System.out.println("Cooperator = " + finalCooperator);
+    System.out.println("Defector = " + finalDefector);
+    System.out.println("PartialCooperator = " + finalPartial);
+    System.out.println();
+    System.out.println("Mean Cooperation Probability = " + p.calculateCooperationMean());    
+  }
+}
--- Assignment4/src/simulation/Population.java
+++ Assignment4/src/simulation/Population.java
@@ -1,55 +1,111 @@
 package simulation;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Random;
 
 public class Population {
   private Organism[] organism;
   
   public Population(Pair<String, Integer>[] counts) throws IllegalArgumentException {
-    List<Organism> arrayList = new ArrayList<>();
+    LinkedList<Organism> list = new LinkedList<>();
 
     for (Pair<String, Integer> pair : counts) {
-      for (int i = 0; i < pair.getRight(); i++) {
-        if (pair.getLeft().equals("Cooperator")) {
-          arrayList.add(new Cooperator());
-        } else if (pair.getLeft().equals("Defector")) {
-          arrayList.add(new Defector());
-        } else if (pair.getLeft().equals("PartialCooperator")) {
-          arrayList.add(new PartialCooperator());
+      for (int i = 0; i < pair.right; i++) {
+        if (pair.left.equals("Cooperator")) {
+          list.add(new Cooperator());
+        } else if (pair.left.equals("Defector")) {
+          list.add(new Defector());
+        } else if (pair.left.equals("PartialCooperator")) {
+          list.add(new PartialCooperator());
         } else {
           throw new IllegalArgumentException("non-exist organism: " + pair.getLeft());
         }
       }
     }
-    
-    organism = (Organism[])arrayList.toArray();
+        
+    organism = (Organism[]) list.toArray();
   }
   
   public void update() {
+    Random rand = new Random();
     for (int i = 0; i < organism.length; i++) {
+      /* update all of the organism */
       organism[i].update();
-      if (organism[i].cooperates()) {
+      
+      /* check cooperation */
+      if (organism[i].cooperates() && organism[i].getEnergy() >= 1) {
+        // cooperate with other organism
         organism[i].decrementEnergy();
-        
+        int[] randOrg = getRandomNumExcept(organism.length, 8, i);
+        for (int j = 0; j < randOrg.length; j++) {
+          organism[randOrg[j]].incrementEnergy();
+        }
+      }
+      
+      /* check reproduce */
+      if (organism[i].getEnergy() >= 10) {
+        int organismToReplace = rand.nextInt(organism.length + 1);
+        organism[organismToReplace] = organism[i].reproduce();
       }
     }
   }
   
-  private static int[] getNonDuplicateRandom(int capacity, int num) {
-    int[] random = new int[capacity];
-    for (int i = 0; i < capacity; i++) {
-      random[i] = i;
+  // to do: overflow potential
+  public double calculateCooperationMean() {
+    double average = 0.0;
+    for (int i = 0; i < organism.length; i++) {
+      average += organism[i].getCooperationProbability();
     }
-    
-    Random r = new Random();
-    for (int i = random.length - 1; i >= 0; i--) {
-      int indexToSwap = r.nextInt(i + 1);
-      swap(random, indexToSwap, i);
-    }
+    return average / organism.length;
   }
   
+  public Pair<String, Integer>[] getPopulationCounts() {
+    /* map all organism into a dictionary */
+    HashMap<String, Integer> dic = new HashMap<String, Integer>();
+    for (int i = 0; i < organism.length; i++) {
+      String orgName = organism[i].getType();
+      if (dic.containsKey(orgName)) {
+        Integer num = dic.get(orgName);
+        dic.put(orgName, num + 1);
+      } else {
+        dic.put(orgName, 1);
+      }
+    }
+    
+    /* generate pair array */
+    Pair<String, Integer>[] orgPair = (Pair<String, Integer>[]) new Object[dic.keySet().size()];
+    int index = 0;
+    for (String name : dic.keySet()) {
+      Integer num = dic.get(name);
+      orgPair[index] = new Pair<String, Integer>(name, num);
+      index++;
+    }
+    
+    return orgPair;
+  }
   
+  private static int[] getRandomNumExcept(int scope, int num, int except) {
+    int[] randArray = new int[num];
+    Random rand = new Random();
+    
+    for (int i = 0; i < num; i++) {
+      int randInt = rand.nextInt(scope + 1);
+      while (contains(randArray, randInt) || randInt == except) {
+        randInt = rand.nextInt(scope + 1);
+      }
+      randArray[i] = randInt;
+    }
+    
+    return randArray;
+  }
+  
+  private static boolean contains(int[] array, int val) {
+    for (int i = 0; i < array.length; i++) {
+      if (array[i] == val)
+        return true;
+    }
+    return false;
+  }
 }
 
